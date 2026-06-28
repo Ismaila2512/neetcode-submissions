d36fe1a39c252b39e1c1654c6f1a820283bee5f4
Mohammed Ismail VP <ismailqudsi2004@gmail.com>
2026-06-28 12:40:47 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            // Update the minimum price seen so far
+            if (price < minPrice) {
+                minPrice = price;
+            } 
+            // Calculate potential profit and update maxProfit
+            else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
